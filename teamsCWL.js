9ab8ffbbc9afa286b69fcd493cef31b2f4b8a415
giannini.benoit <giannini.benoit@gmail.com>
2024-06-09 10:18:32 +0000

    ajout logo equipe dans un tableau avec le nom et le lien de l'image dans le dossier, ajout des bases du code du site ainsi que la charte graphique du site.

---
+++ teamsCWL.js
@@ -0,0 +1,124 @@
+const teamsWestCWL = [
+{
+    nom: "Dallas Mavericks",
+    image: "image/Mavs.png"
+},
+{
+    nom: "Denver Nuggets",
+    image: "image/Denver.png"
+},
+{
+    nom: "Golden State Warriors",
+    image: "image/GS.png"
+},
+{
+    nom: "Houston Rockets",
+    image: "image/Houston.png"
+},
+{
+    nom: "Los Angeles Clippers",
+    image: "image/Clippers.png"
+},
+{
+    nom: "Los Angeles Lakers",
+    image: "image/Lakers.png"
+},
+{
+    nom: "Memphis Grizzlies",
+    image: "image/Memphis.png"
+},
+{
+    nom: "Minnesota Timberwolves",
+    image: "image/Minny.png"
+},
+{
+    nom: "New Orleans Pelicans",
+    image: "image/Pels.png"
+},
+{
+    nom: "Oklahoma City Thunder",
+    image: "image/OKC.png"
+},
+{
+    nom: "Phoenix Suns",
+    image: "image/Suns.png"
+},
+{
+    nom: "Portland Trail Blazers",
+    image: "image/Portland.png"
+},
+{
+    nom: "Sacramento Kings",
+    image: "image/Kings.png"
+},
+{
+    nom: "San Antonio Spurs",
+    image: "image/Spurs.png"
+},
+{
+    nom: "Utah Jazz",
+    image: "image/Jazz.png"
+}
+];
+const teamsEastCWL = [
+{
+    nom: "Atlanta Hawks",
+    image: "image/Atlanta.png"
+},
+{
+    nom: "Boston Celtics",
+    image: "image/Boston.png"
+},
+{
+    nom: "Brooklyn Nets",
+    image: "image/Brooklyn.png"
+},
+{
+    nom: "Charlotte Hornets",
+    image: "image/Hornets.png"
+},
+{
+    nom: "Chicago Bulls",
+    image: "image/Bulls.png"
+},
+{
+    nom: "Cleveland Cavaliers",
+    image: "image/Cleveland.png"
+},
+{
+    nom: "Detroit Pistons",
+    image: "image/Detroit.png"
+},
+{
+    nom: "Indiana Pacers",
+    image: "image/Pacers.png"
+},
+{
+    nom: "Miami Heat",
+    image: "image/Heat.png"
+},
+{
+    nom: "Milwaukee Bucks",
+    image: "image/Bucks.png"
+},
+{
+    nom: "New York Knicks",
+    image: "image/Knicks.png"
+},
+{
+    nom: "Orlando Magic",
+    image: "image/Orlando.png"
+},
+{
+    nom: "Philadelphia 76ers",
+    image: "image/76ers.png"
+},
+{
+    nom: "Toronto Raptors",
+    image: "image/Toronto.png"
+},
+{
+    nom: "Washington Wizards",
+    image: "image/Washington.png"
+}
+]
